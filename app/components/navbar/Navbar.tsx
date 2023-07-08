e37a6f49eb1934a1c7ebaeacc0147e530ec23c41
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-08 19:23:25 +0530

    some ui changes

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
@@ -6,21 +7,10 @@ import UserMenu from "./UserMenu";
 const Navbar = () => {
 	return (
 		<div className='fixed w-full bg-white z-10 shadow-sm'>
-			<div
-				className='
-          py-4 
-          border-b-[1px]
-        '
-			>
+			<div className='py-4 border-b-[1px]'>
 				<Container>
 					<div
-						className='
-            flex 
-            flex-row 
-            items-center 
-            justify-between
-            gap-3
-            md:gap-0
+						className='flex flex-row items-center justify-between gap-3 md:gap-0
           '
 					>
 						<Logo />
