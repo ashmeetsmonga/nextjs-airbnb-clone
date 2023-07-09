6b4b02afcb95213ba57e37378e8dd30ab503208b
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-09 17:14:13 +0530

    added menuitems on login and signout func

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,12 +2,18 @@
 
 import useLoginModal from "@/app/hooks/useLoginModal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import { User } from "@prisma/client";
 import { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
+import { signOut } from "next-auth/react";
 
-const UserMenu = () => {
+interface UserMenuProps {
+	currentUser?: User | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 
 	const registerModal = useRegisterModal();
@@ -34,10 +40,22 @@ const UserMenu = () => {
 			{isOpen && (
 				<div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
 					<div className='flex flex-col cursor-pointer'>
-						<>
-							<MenuItem label='Login' onClick={loginModal.onOpen} />
-							<MenuItem label='Signup' onClick={registerModal.onOpen} />
-						</>
+						{currentUser ? (
+							<>
+								<MenuItem label='My Trips' onClick={() => {}} />
+								<MenuItem label='My Favourites' onClick={() => {}} />
+								<MenuItem label='My Reservations' onClick={() => {}} />
+								<MenuItem label='My Properties' onClick={() => {}} />
+								<MenuItem label='Airbnb My Home' onClick={() => {}} />
+								<hr />
+								<MenuItem label='Logout' onClick={() => signOut()} />
+							</>
+						) : (
+							<>
+								<MenuItem label='Login' onClick={loginModal.onOpen} />
+								<MenuItem label='Signup' onClick={registerModal.onOpen} />
+							</>
+						)}
 					</div>
 				</div>
 			)}
